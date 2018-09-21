e81126533b955e93632d0cff680d3e5cba0cf42a
makchamp <makramadaime@hotmail.com>
2018-09-21 03:33:18 -0400

    changed theme

--- app/src/main/java/makchamp/workoutlog/History.java
+++ app/src/main/java/makchamp/workoutlog/History.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.widget.PopupMenu;
 import android.widget.TextView;
 
@@ -47,6 +48,12 @@ public class History extends AppCompatActivity {
 
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.history_menu, menu);
+        return true;
     }
 
 
